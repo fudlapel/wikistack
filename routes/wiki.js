022fe5d5c6c3c5560d17d4a70aba591418675602
Michelle Messenger <michelle.messenger719@gmail.com>
2018-08-03 13:03:53 -0400

    git commit

---
+++ routes/wiki.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  res.send('got to GET /wiki/')
+});
+
+router.post("/", async (req, res, next) => {
+  res.send('got to POST /wiki/')
+});
+
+router.get("/add", async (req, res, next) => {
+  res.send('got to GET /wiki/add')
+});
+
+module.exports = router
